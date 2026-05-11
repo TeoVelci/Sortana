feceb50b9d3fc4f3b0bfb138734e10ea211b5052
Matteo Velci <matteovelci@MacBook-Pro-2.local>
2026-05-11 04:01:42 -0700

    chore: remove test files with hardcoded keys to fix Netlify deployment

--- test-edit.js
+++
@@ -1,5 +0,0 @@
-import { GoogleGenAI } from '@google/genai';
-import fs from 'fs';
-
-const apiKey = process.env.VITE_GEMINI_API_KEY || "AIzaSy..."; 
-// we will load it from .env or netlify.toml
--- test-magic.js
+++
@@ -1,3 +0,0 @@
-import fs from 'fs';
-const base64Data = fs.readFileSync('test.jpg', 'base64');
-console.log(base64Data.substring(0, 50));
